8ff0aca49ac7b8dd0afdb4c287fc13564410ed94
Luiz Gabriel <gabrielon6689@gmail.com>
2025-08-04 22:17:40 -0300

    Enable CORS with specified HTTP methods in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({ methods })
 }
 bootstrap();
