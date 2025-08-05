049bccce239c8ae4a87fb37939202edd5623f887
Luiz Gabriel <gabrielon6689@gmail.com>
2025-08-04 22:09:24 -0300

    Add @nestjs/mapped-types dependency and import UsersModule in app.module.ts

---
+++ src/users/users.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Put,
+} from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { Event, Role, User } from "generated/prisma";
+
+@Controller("/users")
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(
+    @Body()
+    userData: {
+      email: string;
+      name: string;
+      picture: string;
+      role: Role;
+      created_at: string;
+      token: string;
+      event: Event[];
+    }
+  ): Promise<User> {
+    return this.usersService.create(userData);
+  }
+
+  @Get()
+  async findUsers(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(":id")
+  async findUserById(@Param("id") id: string): Promise<User | null> {
+    return this.usersService.findOne(Number(id));
+  }
+
+  // @Put(':id')
+  // async updateUser(@Param('id') id: string, @Body() userData: { name?: string }): Promise<User> {
+  //   return this.usersService.updateUser(Number(id), userData);
+  // }
+
+  @Delete(":id")
+  async deleteUser(@Param("id") id: string): Promise<User> {
+    return this.usersService.deleteUser(Number(id));
+  }
+}
